36e057dd28c6bdbdf22ffea83f2979acd8cddd7f
kennyrogers92@gmail.com <128783334+kennyrogers92@users.noreply.github.com>
2024-03-29 21:07:35 +0800

    polyinterp module commit

--- lectures/lagrange.py
+++ lectures/lagrange.py
@@ -1,34 +1,36 @@
-# Sample implementation of Lagrange Interpolation
+# Sample implementation of Polynomial interpolation methods
 
 import numpy as np
-from matplotlib import pyplot as plt
-from polyinterp import lagrange
+from polyinterp import lagrange, newtonlagrange, hermite, chebyshev, legendre
 
-def fun(x):
+def f(x):
     return np.e**(3*x)*np.sin(2*np.pi*x)
 
-x = np.linspace(0., 1., 100)
-y = fun(x)
+def df(x):
+    return np.e**(3*x)*(3*np.sin(2*np.pi*x) + 2*np.pi*np.cos(2*np.pi*x))
 
-cubic = lagrange(fun, [1./3.*_ for _ in range(4)], x)
-quartic = lagrange(fun, [1./4.*_ for _ in range(5)], x)
+def g(x):
+    return 1./(1. + x**2)
 
-fig = plt.figure()
-ax1 = fig.add_subplot(121)
-ax2 = fig.add_subplot(122)
+x = [1./3.*i for i in range(4)]
+print("Interpolation of exp(3x)sin(2pix) in the interval [0, 1] with 4 ",
+      "equally-spaced nodes:")
+print(f"Via Lagrange:\t\t", lagrange(f, x))
+print(f"Via Newton-Lagrange:\t", newtonlagrange(f, x))
 
-ax1.grid(color="gray", linestyle="--", linewidth=0.5)
-ax1.plot(x, y, 'r', label=r"$f$")
-ax1.plot(x, cubic, label=r"$L_{f, 3}$")
-ax1.scatter(np.linspace(0., 1., 4), fun(np.linspace(0., 1., 4)), s=15, marker="o", c="k")
-ax1.legend(loc='upper right')
+x = [1./4.*i for i in range(5)]
+print("Interpolation of exp(3x)sin(2pix) in the interval [0, 1] with 5 ",
+      "equally-spaced nodes:")
+print(f"Via Lagrange:\t\t", lagrange(f, x))
+print(f"Via Newton-Lagrange:\t", newtonlagrange(f, x))
 
-ax2.grid(color="gray", linestyle="--", linewidth=0.5)
-ax2.plot(x, y, 'r', label=r"$f$")
-ax2.plot(x, quartic, label=r"$L_{f, 4}$")
-ax2.scatter(np.linspace(0., 1., 5), fun(np.linspace(0., 1., 5)), s=15, marker="o", c="k")
-ax2.legend(loc='upper right')
+print("Interpolation of exp(3x)sin(2pix) in the interval [0, 1] with 2 ",
+      "equally-spaced nodes:")
+print(f"Via Hermite with 2 nodes:\t\t", hermite(f, df, [0., 1.]))
+print(f"Via Hermite with 3 nodes:\t\t", hermite(f, df, [0., 0.5, 1.]))
 
-# plt.savefig('plots/lagrange.png', format='png', dpi=300)
-plt.show()
-plt.gca().set_aspect('equal')
+print("Interpolation of 1/(1 + x^2) in the interval [-5, 5]\n",
+      "with nodes:")
+print("Via Chebyshev:\t", chebyshev(g, 15, -5, 5))
+print("Via Legendre:\t", legendre(g, 15, -5, 5))
+
--- lectures/polyinterp.py
+++ lectures/polyinterp.py
@@ -1,9 +1,116 @@
 "Module for polynomial interpolations"
 
 from linalg import vector, matrix, solveLU
+from rootspoly import param, newtonhorner
 import numpy as np
 
-def undeterminedcoeff(f: callable, x: vector) -> list:
+class polynomial(list):
+    """
+    Class of polynomials where polynomial of the form
+        a0 + a1x + a2x^2 + ... + anx^2
+        is stored in a list 
+        [a0, a1, a2, ..., an]
+    """
+    def __init__(self, iterable=list()):
+        if not iterable:
+            super().__init__()
+        else:
+            super().__init__(iterable)
+    
+    def __setitem__(self, index, item):
+        super().__setitem__(index, item)
+        
+    def __getitem__(self, index):
+        return super().__getitem__(index)
+    
+    def __str__(self):
+        os = ""
+        n = len(self)
+        for k in range(n- 1, -1, -1):
+            coeff = self[k]
+            if coeff == 0.:
+                continue
+            elif coeff < 0.:
+                coeff *= -1.
+                if k == n - 1:
+                    os += "-"
+                else:
+                    os += " - "
+            else:
+                if k < n - 1:
+                    os += " + "
+            if coeff != 1. or k == 0:
+                os += "{:.30f}".format(coeff)
+            if k == 1:
+                os += "x"
+            if k > 1:
+                os += f"x^{k}"
+        return os
+
+    def __add__(self, other):
+        n, m = len(self), len(other)
+        deg = max(n, m)
+        C = polynomial()
+        for i in range(deg):
+            if i < n:
+                a = self[i]
+            else:
+                a = 0.
+            if i < m:
+                b = other[i]
+            else:
+                b = 0
+            C.append(a + b)
+        return C
+    
+    __radd__ = __add__
+    
+    def __neg__(self):
+        n = len(self)
+        other = polynomial()
+        for i in range(n):
+            other.append(-self[i])
+        return other
+
+    def __sub__(self, other):
+        return self + (-other)
+    
+    def __rsub__(self, other):
+        return other - self
+    
+    def __mul__(self, other):
+        if type(other) == int or type(other) == float:
+            n = len(self)
+            C = polynomial()
+            for i in range(n):
+                C.append(self[i]*other)
+            return C
+        elif type(other) == polynomial:
+            C = polynomial()
+            n = len(self) + len(other) - 2
+            for k in range(n + 1):
+                c = 0
+                for j in range(k + 1):
+                    if j < len(self) and k - j < len(other):
+                        c += self[j]*other[k - j]
+                C.append(c)
+            return C
+        else:
+            return NotImplemented
+
+    def __rmul__(self, other):
+        if type(other) == int or type(other) == float:
+            return self*other
+        else:
+            return NotImplemented
+    
+    def __truediv__(self, other):
+        if type(other) == int or type(other) == float:
+            return self*(1./other)
+        else:
+            return NotImplemented
+
+def undeterminedcoeff(f: callable, x: vector) -> polynomial:
     """
     Finds the coefficients of the interpolating polynomial on f
     given the interpolation nodes x by the Method of
@@ -31,134 +138,172 @@ def undeterminedcoeff(f: callable, x: vector) -> list:
     b = vector()
     for i in range(n):
         b.append(f(x[i]))
-    return solveLU(V, b).x
+    return polynomial(solveLU(V, b).x)
 
-
-def lagrange(f, x, z):
+def lagrange(f: callable, x: vector) -> polynomial:
     """
-    Lagrange polynomial interpolation of f at nodes x evaluated at points z
-    
-    Attributes
+    Lagrange polynomial interpolation of f at nodes x
+
+    Parameters
     ----------
-        f : callable
-            objective real valued function
-        x : list
-            nodes to interpolate function at
-        z : list
-            values of Lagrange interpolation polynomial
+    f : callable
+        objective real valued function
+    x : vector
+        nodes to interpolate function at
+
+    Returns
+    -------
+    polynomial
+        interpolating polynomial of f at nodes x
     """
     n = len(x)
-    m = len(z)
-    v = []
-    for j in range(m):
-        val = 0
-        for k in range(n):
-            l = 1
-            for i in range(n):
-                if i == k:
-                    continue
-                l = l * (z[j] - x[i]) / (x[k] - x[i])
-            val = val + f(x[k]) * l
-        v.append(val)
-    return v
+    p = polynomial([0])
+    for k in range(n):
+        l = polynomial([1])
+        for i in range(n):
+            if i == k:
+                continue
+            temp = polynomial([-x[i], 1])
+            temp = temp / (x[k] - x[i])
+            l = l * temp
+        p = p + f(x[k])*l 
+    return p
 
-
-def newtonlagrange(f, x, z):
+def newtonlagrange(f: callable, x: vector) -> polynomial:
     """
-    Newton-Lagrange polynomial interpolation of f at nodes x evaluated at points z
-    
-    Attributes
+    Newton-Lagrange polynomial interpolation of f at nodes x
+
+    Parameters
     ----------
-        f : callable
-            objective real valued function
-        x : list
-            nodes to interpolate function at
-        z : list
-            values of Newton-Lagrange interpolation polynomial
+    f : callable
+        objective real valued function
+    x : vector
+        nodes to interpolate function at
+
+    Returns
+    -------
+    polynomial
+        
     """
     n = len(x)
-    m = len(z)
-    v = [0] * m
-    D = [[0] * n for _ in range(n)]
+    D = matrix([[0]*n for _ in range(n)])
     for k in range(n):
         D[k][0] = f(x[k])
     for k in range(1 , n):
         for l in range(k, n):
             D[l][k] = (D[l][k - 1] - D[l - 1][k - 1]) / (x[l] - x[l - k])
-    for j in range(m):
-        v[j] = D[n - 1][n - 1]
-        for k in range(1, n):
-            v[j] = v[j] * (z[j] - x[n - k - 1]) + D[n - k - 1][n - k - 1]
-    return v
+    p = polynomial([D[n - 1][n - 1]])
+    for k in range(1, n):
+        temp = polynomial([-x[n - k - 1], 1])
+        p = p*temp + polynomial([D[n - k - 1][n - k - 1]])
+    return p
 
-
-def hermite(f, df, x, z):
+def hermite(f: callable, df: callable, x: vector) -> polynomial:
     """
-    Hermite polynomial interpolation of f at nodes x evaluated at points z
-    
-    Attributes
+    Hermite polynomial interpolation of f at nodes x
+
+    Parameters
     ----------
-        f : callable
-            objective real valued function
-        df : callable
-            derivative of f
-        x : list
-            nodes to interpolate function at
-        z : list
-            values of Hermite interpolation polynomial
+    f : callable
+        objective real valued function
+    df : callable
+        derivative of f
+    x : vector
+        nodes to interpolate function at
+
+    Returns
+    -------
+    polynomial
+        interpolating polynomial of f at nodes x
     """
     n = len(x)
-    m = len(z)
-    v = [0] * m
-    for j in range(m):
-        for k in range(n):
-            delta = 1
-            nu = 1
-            l1 = 0
-            for i in range(n):
-                if i == k:
-                    continue
-                nu = nu * (z[j] - x[i])
-                delta = delta * (x[k] - x[i])
-                l1 = l1 + 1 / (x[k] - x[i])
-            l2 = (nu / delta)**2
-            eta = (z[j] - x[k]) * l2 
-            h = (1 - 2 * l1*(z[j] - x[k])) * l2
-            v[j] = v[j] + f(x[k]) * h + df(x[k]) * eta
-    return v
+    p = polynomial([0])
+    for k in range(n):
+        delta = 1
+        nu = polynomial([1])
+        ell1 = 0
+        for i in range(n):
+            if i == k:
+                continue
+            nu = nu * polynomial([-x[i], 1])
+            delta = delta * (x[k] - x[i])
+            ell1 = ell1 + 1. / (x[k] - x[i])
+        ell2 = (nu/delta)*(nu/delta)
+        eta = polynomial([-x[k], 1]) * ell2 
+        h = (polynomial([1]) - 2.*ell1*polynomial([-x[k], 1])) * ell2
+        p = p + f(x[k]) * h + df(x[k]) * eta
+    return p
 
-
-def piecewise(f, x, z, p):
+def chebyshev(f: callable, n: int, a: float=0., b: float=1.) -> polynomial:
     """
-    Piecewise Lagrange polynomial interpolation of f at nodes x evaluated at points z
-        where p is the degree of each interpolating polynomial at each interval
-    
-    Attributes
+    Lagrange intepolating polynomial at the n affine-transformed Chebyshev points
+
+    Parameters
     ----------
-        f : callable
-            objective real valued function
-        x : list
-            nodes to interpolate function at
-        z : list
-            values of Lagrange interpolation polynomial
-        p : int
-            degree of interpolating polynomial
+    f : callable
+        objective real valued function
+    n : int
+        one less the number of chebyshev points
+    a : float, optional
+        left end point of the polynomial, by default 0.
+    b : float, optional
+        right end point of the polynomial, by default 1.
+
+    Returns
+    -------
+    polynomial
+        interpolating polynomial of f at n Chebyshev
     """
-    n = len(x)
-    m = len(z)
-    v = []
-    j = 0
-    for i in range(n - p):
-        xi = [x[i + _] for _ in range(p + 1)]
-        zi = []
-        if z[j] < xi[0]:
-            j += 1
-        while z[j] <= xi[-1]:
-            zi.append(z[j])
-            j += 1
-            if j == m:
-                break
-        print(len(zi))
-        v.extend(lagrange(f, xi, zi))
-    return v
-    
+    x = vector()
+    for k in range(n+1):
+        temp = np.cos((k+0.5)*np.pi/(n+1.))
+        temp = 0.5*((b - a)*temp + a + b)
+        x.append(temp)
+    return newtonlagrange(f, x)
+
+def legendre_poly(n: int) -> polynomial:
+    """
+    Construct the nth Legendre Polynomial
+
+    Parameters
+    ----------
+    n : int
+        order of the Polynomial
+
+    Returns
+    -------
+    polynomial
+        nth order Legendre Polynomial
+    """
+    if n == 0:
+        return polynomial([1.])
+    if n == 1:
+        return polynomial([0., 1.])
+    return polynomial([0., (2. - 1./n)]) * legendre_poly(n - 1)\
+        + polynomial([-1. + 1./n]) * legendre_poly(n - 2)
+
+def legendre(f: callable, n: int, a: float=0., b: float=1.) -> polynomial:
+    """
+    Lagrange Interpolating polynomial at the n affine-transformed Legendre points
+
+    Parameters
+    ----------
+    f : callable
+        objective real valued function
+    n : int
+        one less the number of chebyshev points
+    a : float, optional
+        left end point of the polynomial, by default 0.
+    b : float, optional
+        right end point of the polynomial, by default 1.
+
+    Returns
+    -------
+    polynomial
+        interpolating polynomial of f at n legendre points
+    """
+    p = legendre_poly(n + 1)
+    z = newtonhorner(p, a+b/2., param()).z
+    x = [0.5*(b - a)*y.real + a + b for y in z]
+    print(x)
+    return newtonlagrange(f, x)
